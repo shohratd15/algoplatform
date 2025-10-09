55e36641d5674058494a6f3e1ea88772b8847da1
shohratd15 <shohratd15@gmail.com>
2025-10-09 15:42:06 +0500

    fixed bugs

--- migrations/000004_init_submissions_schema.up.sql
+++ migrations/000004_init_submissions_schema.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE submissions (
     problem_id  BIGINT REFERENCES problems(id) ON DELETE CASCADE,
     language_id INTEGER NOT NULL,
     source_code TEXT NOT NULL,
-    status TEXT NOT NULL CHECK (status IN ('queued','running','accepted','wrong_answer','runtime_error','error','time_limit','memory_limit'))
+    status TEXT NOT NULL CHECK (status IN ('queued','running','accepted','wrong_answer','runtime_error','error','time_limit','memory_limit')),
     created_at  TIMESTAMP DEFAULT now(),
-    updated_at   TIMESTAMP 
+    updated_at  TIMESTAMP 
 );
